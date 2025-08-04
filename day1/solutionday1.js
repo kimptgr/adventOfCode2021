aa117e76331aa5235834a28423c34168cdfc781b
kimptgr <patingrekim@gmail.com>
2025-08-04 13:40:58 +0200

    add part two

--- day1/solutionday1.js
+++ day1/solutionday1.js
@@ -11,7 +11,7 @@ import { readFileSync } from "fs";
 function readLinesFromFile(inputPath) {
   try {
     const input = readFileSync(new URL(inputPath, import.meta.url), "utf-8");
-    return input.trim().split("\r?\n");
+    return input.trim().split(/\r?\n/);
   } catch (error) {
     console.error(`Error reading file ${inputPath}: `, error);
     return [];
@@ -47,4 +47,44 @@ export function resolvePartOne() {
   return message;
 }
 
+/**
+ * Create an Array with sums of a three-measurement sliding window
+ * @param {string[]|Number[]} depths Array representing numeric depths.
+ * @returns {Number[]} Array representing the sums of a three-measurement sliding window
+ */
+export function getSlidingWindowsSums(depths) {
+  const sizeOfWindow = 3;
+  let sum = 0;
+  let slidingWindow = [];
+  let sumOfMeasurements = [];
+
+  for (let i = 0; i < depths.length; i++) {
+    const depth = Number(depths[i]);
+    slidingWindow.push(depth);
+    sum += depth;
+
+    if (slidingWindow.length === sizeOfWindow) sumOfMeasurements.push(sum);
+    if (slidingWindow.length >= sizeOfWindow) {
+      sum -= slidingWindow[0];
+      slidingWindow.shift();
+    }
+  }
+  return sumOfMeasurements;
+}
+
+/**
+ * Resolve part two of the problem Advent of Code Day 1 by counting increases in the input file.
+ * @returns {string} Result message.
+ */
+export function resolvePartTwo() {
+  const lines = readLinesFromFile("input.txt");
+  const measurements = getSlidingWindowsSums(lines);
+  const count = countIncreased(measurements);
+  const message = `Number of sums in a three-measurement sliding window larger than the previous one: ${count}`;
+  console.log(message);
+
+  return message;
+}
+
 resolvePartOne();
+resolvePartTwo();
--- day1/solutionday1.test.js
+++ day1/solutionday1.test.js
@@ -1,4 +1,17 @@
-import { countIncreased } from "./solutionday1.js";
+import { countIncreased, getSlidingWindowsSums } from "./solutionday1.js";
+
+const AdventOfCodeExample = [
+  "199",
+  "200",
+  "208",
+  "210",
+  "200",
+  "207",
+  "240",
+  "269",
+  "260",
+  "263",
+];
 test("returns 0 for an empty array", () => {
   expect(countIncreased([])).toBe(0);
 });
@@ -12,18 +25,17 @@ test("returns 1 when the second number is higher", () => {
 });
 
 test("counts how many measurements are larger than the previous one in the Advent of Code example.", () => {
-  expect(
-    countIncreased([
-      "199",
-      "200",
-      "208",
-      "210",
-      "200",
-      "207",
-      "240",
-      "269",
-      "260",
-      "263",
-    ])
-  ).toBe(7);
+  expect(countIncreased(AdventOfCodeExample)).toBe(7);
+});
+
+test("returns an empty array if there are fewer than 3 elements", () => {
+  expect(getSlidingWindowsSums([])).toStrictEqual([]);
+  expect(getSlidingWindowsSums(["100"])).toStrictEqual([]);
+  expect(getSlidingWindowsSums(["100", "200"])).toStrictEqual([]);
+});
+
+test("returns the sum of each three-measurement sliding window", () => {
+  expect(getSlidingWindowsSums(AdventOfCodeExample)).toStrictEqual([
+    607, 618, 618, 617, 647, 716, 769, 792,
+  ]);
 });
