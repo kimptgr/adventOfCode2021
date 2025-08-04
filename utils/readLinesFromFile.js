dafe39c682a0ca5a2b68bdb8b9659ad99c9b5574
kimptgr <patingrekim@gmail.com>
2025-08-04 14:43:19 +0200

    refactor readLinesFromFiles

---
+++ utils/readLinesFromFile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import { readFileSync } from "fs";
+
+/**
+ * Reads a text file and returns an array of lines
+ *
+ * @param {string} inputPath Relative path to the input file.
+ * @returns {string[]} Array of lines from the file.
+ */
+export function readLinesFromFile(inputPath) {
+  console.log(inputPath);
+  try {
+    const input = readFileSync(inputPath, "utf-8");
+    return input.trim().split(/\r?\n/);
+  } catch (error) {
+    console.error(`Error reading file ${inputPath}: `, error);
+    return [];
+  }
+}
